d4f178c1f14b66c3593626d64ecfc8abbe303d2f
Nicolas Bresson <nico@Nicolass-MBP.fritz.box>
2023-07-26 22:26:41 +0200

    port as env variable

--- src/index.js
+++ src/index.js
@@ -87,6 +87,8 @@ app.post("/posts", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
